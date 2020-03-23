e111ca555a4a9317769ee0d8b4d09ad9b37060cd
GemsGame <a.masanovets@gmail.com>
2020-03-24 01:27:38 +0300

    feat: add promise to resourses battle loader

--- src/observer/index.js
+++ src/observer/index.js
@@ -10,15 +10,19 @@ export default function listener() {
         if (event.target.id === "start") {
             store.login.status = false;
             store.loading.loadingStatus = true;
-            store.notify();    
-
-            setTimeout(() => {
-                store.loading.loadingStatus = false;
-                store.battle.status = true;
-                store.notify();  
-            }, 3000); 
+            store.notify();
+            store.battle.preload().then(data => {
+                console.log("battle resourses loaded...100%");
+                setTimeout(() => {
+                    store.loading.loadingStatus = false;
+                    store.battle.status = true;
+                    store.notify();
+                }, 2000);
+            }).catch(data => {
+               throw data;
+            })
         }
-    }) 
+    })
 
     store.login.render();
 }
