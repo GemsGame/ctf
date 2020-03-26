d5b2f855a8f871bcc8938d1b935a98325675a758
GemsGame <a.masanovets@gmail.com>
2020-03-26 23:47:20 +0300

    feat: add find clusters

--- src/components/row/index.js
+++ src/components/row/index.js
@@ -9,19 +9,19 @@ export default class Row {
 
     render() {
         if (this.generation === true) {
-            this.generateRows(5,5);
+            this.generateRows(5, 5);
         }
     }
 
     generateRows(columns, rows) {
         for (let i = 0; i < columns * rows; i++) {
-             let tile = this.generateRandomTile('characters');
-             tile = new PIXI.TilingSprite(tile.texture, 256, 256);
-             tile.scale.set(0.4);
-             tile.anchor.set(0.5);
-             tile.x = (i % rows) * 100;
-             tile.y = Math.floor(i / columns) * 100;
-             this.battleContainer.addChild(tile);
+            let tile = this.generateRandomTile('characters');
+            tile = new PIXI.TilingSprite(tile.texture, 256, 256);
+            tile.scale.set(0.4);
+            tile.anchor.set(0.5);
+            tile.x = (i % rows) * 100;
+            tile.y = Math.floor(i / columns) * 100;
+            this.battleContainer.addChild(tile);
         }
 
 
@@ -37,17 +37,89 @@ export default class Row {
             this.battleContainer.pivot.x = this.battleContainer.width / 1.7;
             this.battleContainer.pivot.y = this.battleContainer.height / 2;
         }
+
+
+        this.findClusters();
+
+
     }
-     
+
     generateRandomTile(textureName) {
-       let random = [];
-       Object.keys(App.loader.resources).map((item, i) => {
-            if(item.indexOf(textureName) !== -1) {
+        let random = [];
+        Object.keys(App.loader.resources).map((item, i) => {
+            if (item.indexOf(textureName) !== -1) {
                 random.push(App.loader.resources[item]);
-            }        
-       });
-       return random[Math.round(Math.random() * (random.length - 1))];
+            }
+        });
+        return random[Math.round(Math.random() * (random.length - 1))];
     }
+
+    findClusters() {
+        //horisontal cluster 
+        let arr = [
+            [],
+            [],
+            [],
+            [],
+            []
+        ];
+
+        let count = 0;
+        for (let i = 0; i < this.battleContainer.children.length; i++) {
+            if (i === 5 || i === 10 || i === 15 || i === 20) {
+                count += 1;
+            }
+            arr[count].push(this.battleContainer.children[i]);
+        }
+
+        for (var c = 0; c < arr.length; c++) {
+            let clusters = 1;
+            for (var r = 0; r < arr.length; r++) {
+                if (r === arr.length - 1) {
+                    if (clusters >= 3) {
+                        console.log('push cluster', clusters);
+                    }
+                    clusters = 1;
+
+                } else {
+                    if (arr[c][r].texture.textureCacheIds[0] === arr[c][r + 1].texture.textureCacheIds[0]) {
+                        clusters += 1;
+                    } else {
+                        // Different type
+                        if (clusters >= 3) {
+                            console.log('push cluster', clusters);
+                        }
+                        clusters = 1;
+                    }
+                }
+            }
+        }
+        //find vartical clusters
+        for (var c = 0; c < arr.length; c++) {
+            let clusters = 1;
+            for (var r = 0; r < arr.length; r++) {
+                if (r === arr.length - 1) {
+                    if (clusters >= 3) {
+                        console.log('push cluster', clusters);
+                    }
+                    clusters = 1;
+
+                } else {
+                    if (arr[r][c].texture.textureCacheIds[0] === arr[r + 1][c].texture.textureCacheIds[0]) {
+                        clusters += 1;
+                    } else {
+                        // Different type
+                        if (clusters >= 3) {
+                            console.log('push cluster', clusters);
+                        }
+                        clusters = 1;
+                    }
+                }
+            }
+        }
+    }
+
+
 
     update(store) {
         this.battleContainer = store.battleContainer.battleContainer;
